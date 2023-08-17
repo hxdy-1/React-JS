8ba68c96be571928a9b2a427f4e91cb56636e9ce
hady <hadimomin02@gmail.com>
2023-08-17 16:56:57 +0530

    finished with building and implementation of custom hooks

--- Task App with Custom Hooks/src/App.js
+++ Task App with Custom Hooks/src/App.js
@@ -1,42 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
     const [tasks, setTasks] = useState([]);
 
-    const fetchTasks = async (taskText) => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch(
-                "https://task-app-b00fc-default-rtdb.firebaseio.com/tasks.json"
-            );
+    const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-            if (!response.ok) {
-                throw new Error("Request failed!");
-            }
-
-            const data = await response.json();
-
+    useEffect(() => {
+        const transformTasks = (transformObj) => {
             const loadedTasks = [];
 
-            for (const taskKey in data) {
-                loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+            for (const taskKey in transformObj) {
+                loadedTasks.push({
+                    id: taskKey,
+                    text: transformObj[taskKey].text,
+                });
             }
 
             setTasks(loadedTasks);
-        } catch (err) {
-            setError(err.message || "Something went wrong!");
-        }
-        setIsLoading(false);
-    };
+        };
 
-    useEffect(() => {
-        fetchTasks();
+        fetchTasks(
+            {
+                url: "https://task-app-b00fc-default-rtdb.firebaseio.com/tasks.json",
+            },
+            transformTasks
+        );
     }, []);
 
     const taskAddHandler = (task) => {
